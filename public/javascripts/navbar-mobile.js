d67784a426917943e6a8b5fe9340f36db2894091
santicano <canosantiago404@gmail.com>
2023-11-11 20:42:33 -0300

    añadido index y layout

---
+++ public/javascripts/navbar-mobile.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let navbarToggler = document.querySelector('.navbar-toggler');
+    let navbarCollapse = document.querySelector('.navbar-collapse');
+
+    navbarToggler.addEventListener('click', function() {
+        navbarCollapse.classList.toggle('show');
+    });
+});
